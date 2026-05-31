074f2c7b397ef1812f29a4bf920eaa522cef70a0
Kauã Matheus <kauamatheussilva06@gmail.com>
2026-05-31 11:00:33 -0300

    fix: Test scope

--- api/src/test/java/com/demoapp/demo/service/UserServiceTest.java
+++ api/src/test/java/com/demoapp/demo/service/UserServiceTest.java
@@ -11,12 +11,12 @@ import com.demoapp.demo.repository.UserRepository;
 
 public class UserServiceTest {
 
+    UserService service = new UserService(mock(UserRepository.class));
+
     // --- Email Tests ---
     // Verifica um email válido (true)
     @Test
     void isEmailValid_ReturnsTrueForDefaultEmail() {
-        UserService service = new UserService(mock(UserRepository.class));
-
         assertTrue(service.isEmailValid("kauam@exemplo.com"));
     }
 
@@ -24,32 +24,24 @@ public class UserServiceTest {
     // Email com dois @ (false)
     @Test
     void isEmailValid_ReturnsFalseWithWrongEmails() {
-        UserService service = new UserService(mock(UserRepository.class));
-        
         assertFalse(service.isEmailValid("ama.@2joão@exemplo.com"));
     }
 
     // Email com caracteres especiais (false)
     @Test
     void isEmailValid_ReturnsFalseWithEmailsWithSpecialCharacters() {
-        UserService service = new UserService(mock(UserRepository.class));
-
         assertFalse(service.isEmailValid("matheus%##virgo@g(mail).com"));
     }
 
     // Somente @ (false)
     @Test
     void isEmailValid_ReturnFalseWithOnlyAt() {
-        UserService service = new UserService(mock(UserRepository.class));
-
         assertFalse(service.isEmailValid("@"));
     }
 
     // null (false)
     @Test
     void isEmailValid_ReturnsFalseForNull() {
-        UserService service = new UserService(mock(UserRepository.class));
-
         assertFalse(service.isEmailValid(null));
     }
     
@@ -58,8 +50,6 @@ public class UserServiceTest {
     // Senha no padrão correto (true)
     @Test
     void isPasswordValid_ReturnsTrueForDefaultPassword() {
-        UserService service = new UserService(mock(UserRepository.class));
-
         assertTrue(service.isPasswordValid("Abacax1@"));
     }
 
@@ -67,16 +57,12 @@ public class UserServiceTest {
     // Senha com somente números (false)
     @Test
     void isPasswordValid_ReturnsFalseWithWrongPassword() {
-        UserService service = new UserService(mock(UserRepository.class));
-        
         assertFalse(service.isPasswordValid("123123123"));
     }
 
     // Senha menor do que o necessário (false)
     @Test
     void isPasswordValid_ReturnsFalseWithShorterPassword() {
-        UserService service = new UserService(mock(UserRepository.class));
-        
         assertFalse(service.isPasswordValid("Aaa@111"));
     }
 
